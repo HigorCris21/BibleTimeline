4a6cd0bd45ea4a50d7c2ec4a70d928a190e3273f
Higor  Lo Castro <higorlocastro@MacBook-Pro-de-Higor.local>
2026-02-05 19:48:17 -0300

    Initial commit

---
+++ BibleTimeline/BibleTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleTimelineApp.swift
+//  BibleTimeline
+//
+//  Created by Higor  Lo Castro on 05/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BibleTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
